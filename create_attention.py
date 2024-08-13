524a5bdf2315b1bd70c8291df53f4d6ac3209ecd
Mattia Gualtieri <mattiagualtieri2000@gmail.com>
2024-08-13 14:43:26 +0300

    Chunked processing

--- create_attention.py
+++ create_attention.py
@@ -20,7 +20,7 @@ def create_attention(args: dict):
 
     patches_coords = args['patches_coords']
     with h5py.File(patches_coords, 'r') as f:
-        coords = f['coords'][:20000]
+        coords = f['coords'][:1000]
     total_patches = len(coords)
     print(f'Successfully loaded {total_patches} patch coordinates from {patches_coords}')
 
@@ -32,29 +32,46 @@ def create_attention(args: dict):
     slide_height = slide.height
     print(f'Successfully loaded {input_slide} slide')
 
-    strip = pyvips.Image.black(slide_width, slide_height)
-    strip = strip.new_from_image([255, 255, 255])
-    strip = strip.copy(interpretation='srgb')
-
     patches_chunk_size = args['patches_chunk_size']
+    chunks = []
     for i in range(0, total_patches, patches_chunk_size):
         chunk_coords = coords[i:i + patches_chunk_size]
-
+        mx = my = Mx = My = -1
+        chunk = pyvips.Image.black(slide_width, slide_height).addalpha()
+        chunk = chunk.new_from_image([255, 255, 255, 0])
+        chunk = chunk.copy(interpretation='srgb')
         for coord in chunk_coords:
             x, y = coord
-            patch = pyvips.Image.black(256, 256)
-            patch = patch.new_from_image([255, 0, 0])
+            if mx < 0 or x < mx:
+                mx = x
+            if my < 0 or y < my:
+                my = y
+            if Mx < 0 or x + 256 > Mx:
+                Mx = x + 256
+            if My < 0 or y + 256 > My:
+                My = y + 256
+            patch = pyvips.Image.black(256, 256).addalpha()
+            patch = patch.new_from_image([255, 0, 0, 255])
             patch = patch.copy(interpretation='srgb')
-            # strip = strip.insert(patch, x, y)
-            strip = strip.merge(patch, 'vertical', x, y)
+            chunk = chunk.insert(patch, x, y)
         print(f'Progress: {i}/{total_patches}')
-        # slide = slide.insert(strip.addalpha(), 0, 0)
+        cropped_chunk = chunk.crop(mx, my, Mx - mx, My - my)
+        chunk_file = f'tmp/{len(chunks)}.png'
+        cropped_chunk.write_to_file(chunk_file)
+        chunks.append({
+            'chunk_file': chunk_file,
+            'x': mx,
+            'y': my
+        })
 
     output_slide = args['output_file']
     print(f'Saving attention slide into {output_slide}')
-    strip.set_progress(True)
-    strip.signal_connect('eval', eval_progress)
-    strip.tiffsave(output_slide, tile=True, pyramid=True, compression='jpeg', Q=80, bigtiff=True)
+    for c in chunks:
+        chunk = pyvips.Image.new_from_file(c['chunk_file'])
+        slide = slide.insert(chunk, c['x'], c['y'])
+    slide.set_progress(True)
+    slide.signal_connect('eval', eval_progress)
+    slide.tiffsave(output_slide, tile=True, pyramid=True, compression='jpeg', Q=80, bigtiff=True)
     print('Attention slide saved!')
 
 
